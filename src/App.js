86227b1c07637709ef14c3584311d7fde02ae076
Meko Steinberg <steinberg.meko@outlook.com>
2022-12-09 10:07:31 -0800

    header and footer(sticky)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,45 @@
 import React from 'react';
-import {AppBar, Container, Paper, Typography} from '@mui/material';
+import { AppBar, Box, Container, Paper, Toolbar, Typography } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CameraIcon from '@mui/icons-material/PhotoCamera';
 
-const App = () =>{
+const theme = createTheme();
+
+const App = () => {
   return (
-    <Container>
-      <Paper>
-        <AppBar position="static" sx={{ m: 2, ml: 0, p: 2, bgcolor: 'secondary' }}>
-          <Typography variant="h3">Rate my Brat!</Typography>
-          <Typography variant="h6">Welcome to Octoberfest</Typography>
-        </AppBar>
-      </Paper>
+    <ThemeProvider theme={theme}>
+      <Container>
+        <Paper>
+          <AppBar position="static" sx={{ m: 2, ml: 0, p: 2, bgcolor: 'secondary' }}>
+            <Toolbar>
+              <CameraIcon sx={{ mr: 2 }} />
+              <Typography variant="h3">Rate my Brat!</Typography>
+              <Typography sx={{ p: 3 }} variant="h6">Welcome to Octoberfest</Typography>
+            </Toolbar>
 
-    </Container> 
+          </AppBar>
+        </Paper>
+
+
+
+        <Box
+          component="footer"
+          sx={{
+            py: 3, px: 2, mt: 'auto',
+            backgroundColor: (theme) =>
+              theme.palette.mode === 'light'
+                ? theme.palette.grey[400]
+                : theme.palette.grey[800],
+          }}
+        >
+          <Container maxWidth="sm">
+            <Typography variant="body1">
+              Brought to you by A.Champion and M.Steinberg
+            </Typography>
+          </Container>
+        </Box>
+      </Container>
+    </ThemeProvider>
   );
 
 }
