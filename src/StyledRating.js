ea00ce548d6e30a05f73de8021e3cb69d1906f25
Meko Steinberg <steinberg.meko@outlook.com>
2022-12-13 23:19:38 -0800

    Component Refactor

---
+++ src/StyledRating.js
@@ -0,0 +1,12 @@
+import Rating from '@mui/material/Rating';
+import { styled } from '@mui/material/styles';
+
+
+export default styled(Rating)({
+    '& .MuiRating-iconFilled': {
+        color: '#ff6d75',
+    },
+    '& .MuiRating-iconHover': {
+        color: '#ff3d47',
+    },
+});
