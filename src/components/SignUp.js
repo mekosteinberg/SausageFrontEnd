5022949569454c9fc716be1d42e28d654e797298
a-champion <a.champion1506@gmail.com>
2022-12-16 14:43:47 -0700

    updated signin/singup

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -13,7 +13,7 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import axios from '../api/axios';
-// import axios from 'axios';
+import axios from 'axios';
 
 function Copyright(props) {
   return (
